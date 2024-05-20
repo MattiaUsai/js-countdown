cf224055586cbafe94592c09da6195067c62d852
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-20 13:36:56 +0200

    finish

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 'use strict';
+console.log('sono connesso')
+
+
+let timer = 10;
+
+const countdown = setInterval(timing ,1000)//imposto unàvariabile count doun che mi attivi la funzione
+
+setTimeout (stop , timer*1000)
+
+
+
+function timing (){
+    console.log('num')
+}
+
+function stop(){
+    console.log('hai raggiunto 10 secondi')
+    alert('buon anno')
+}
